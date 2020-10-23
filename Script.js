efa45441bd9437525a5b6907fdc47860de1cf7a1
Will Jantscher <william.jantscher@gmail.com>
2020-10-23 14:18:57 -0600

    updates

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 //load required modules/packages
 //in console run this to add to the local repository
     //npm install fs wait async
-
+fetch = require('node-fetch')
 fs = require('fs');
 wait = require('wait');
 async = require('async');
@@ -15,14 +15,22 @@ function readFile (file)
     let data = (fs.readFileSync(file).toString()).replace('\r', '').split('\n');  //read in file, convert to string, and split along line
     //if \r are being added to pokemon, remove them
     //console.log(data);
+    return data;
 }
     
-readFile('Pokemon.txt');
+readFile('Pokemon.txt');// For debugging
 
 
 //access pokeapi.co to get each Pokemon's type
 
+var url = "https://pokeapi.co/api/v2/pokemon/"
 
+
+function getPokeURLS(url){
+    let response = await fetch(url);
+    let data = await response.json();
+    data
+}
 
 
 //print each Pokemon with their type to the console
