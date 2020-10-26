6401e1520a93667abe57c55cece3d1425f6f9e07
Will Jantscher <william.jantscher@gmail.com>
2020-10-26 08:15:59 -0600

    added capitalization

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 //load required modules/packages
 //in console run this to add to the local repository
-    //npm install fs wait async
+    //npm install node-fetch fs wait async
 fetch = require('node-fetch')
 fs = require('fs');
 wait = require('wait');
@@ -10,23 +10,25 @@ async = require('async');
 //read in a file of Pokemon names
     //each name seperated by a new line
 
+var dataArr = [];   //put into data array with capitalization
+
 function readFile (file) 
 {
     let data = (fs.readFileSync(file).toString()).replace('\r', '').split('\n');  //read in file, convert to string, and split along line
     //if \r are being added to pokemon, remove them
-    //console.log(data);
-    return data;
+    data.forEach(element => {dataArr.push(element.charAt(0).toUpperCase() + element.slice(1))})
+    console.log(dataArr);
+    return dataArr;
 }
-    
-readFile('Pokemon.txt');// For debugging
 
+readFile('Pokemon.txt');// For debugging
 
 //access pokeapi.co to get each Pokemon's type
 
 var url = "https://pokeapi.co/api/v2/pokemon/"
 
 
-function getPokeURLS(url){
+async function getPokeURLS(url){
     let response = await fetch(url);
     let data = await response.json();
     data
